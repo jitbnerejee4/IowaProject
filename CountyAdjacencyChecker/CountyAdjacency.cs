a2e84202ed0b71af231d98c19d5b1aae7551ba86
jitbanerjee4 <jitbanerjee4@gmail.com>
2024-07-01 01:33:39 -0700

    first-commit

---
+++ CountyAdjacencyChecker/CountyAdjacency.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+public class CountyAdjacency
+{
+    private string connectionString = "Server=localhost;Database=CountyAdjacencyDB;User ID=root;Password=26121990;";
+
+    public int GetCountyID(string countyName)
+    {
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand("SELECT CountyID FROM Counties WHERE CountyName = @CountyName", conn))
+            {
+                cmd.Parameters.AddWithValue("@CountyName", countyName);
+
+                var result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    return Convert.ToInt32(result);
+                }
+                else
+                {
+                    throw new Exception("County not found");
+                }
+            }
+        }
+    }
+
+    public bool AreCountiesAdjacent(int county1, int county2)
+    {
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand("AreCountiesAdjacent", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("County1", county1);
+                cmd.Parameters.AddWithValue("County2", county2);
+
+                MySqlParameter resultParam = new MySqlParameter("Result", MySqlDbType.Bit);
+                resultParam.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(resultParam);
+
+                cmd.ExecuteNonQuery();
+
+                return Convert.ToBoolean(resultParam.Value);
+            }
+        }
+    }
+}
---
+++ CountyAdjacencyChecker/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace CountyAdjacencyChecker
+{
+    public partial class MainWindow : Window
+    {
+        private CountyAdjacency countyAdjacency;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            countyAdjacency = new CountyAdjacency();
+        }
+
+        private void CheckAdjacencyButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int county1 = countyAdjacency.GetCountyID(County1TextBox.Text);
+                int county2 = countyAdjacency.GetCountyID(County2TextBox.Text);
+
+                bool result = countyAdjacency.AreCountiesAdjacent(county1, county2);
+                ResultTextBlock.Text = result ? "Counties are adjacent." : "Counties are not adjacent.";
+            }
+            catch (Exception ex)
+            {
+                ResultTextBlock.Text = ex.Message;
+            }
+        }
+    }
+}
